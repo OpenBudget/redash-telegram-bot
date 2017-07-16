0f667da6fa6464a0f7935d3f58736870db7d3790
VitalyLub <vitlub@gmail.com>
2017-07-16 19:13:27 +0300

    Update telegram_rss.py

--- telegram_rss.py
+++ telegram_rss.py
@@ -75,7 +75,7 @@ for r in rows:
 		#	text_to_send += u'%0A%0Aתקנה: ' + r.get(u'regulation')
 		
 		if r.get(u'volume') is not None and r.get(u'source_currency') is not None:
-				text_to_send += u'%0A*היקף:* ' +  unicode("{:,}".format(r.get(u'volume'))) + u' ' + r.get(u'source_currency')
+				text_to_send += u'%0A*היקף:* ' +  r.get(u'volume') + u' ' + r.get(u'source_currency')
 		
 		if r.get(u'documents') is not None:
 			reg_files = ur"http(.+?)\""
